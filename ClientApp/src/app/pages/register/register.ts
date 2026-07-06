1a98338199fc9988a3c4971554cb8918e410fc8f
Jafor Sadik Tutul <tutul@ucasbd.com>
2026-07-06 18:49:43 +0600

    from start

--- ClientApp/src/app/pages/register/register.ts
+++ ClientApp/src/app/pages/register/register.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  registerForm: FormGroup = new FormGroup({});
+  submitted = false;
+  errorMessage:string[] = [];
+  constructor() {
+    
+  }
+  ngOnInit() {
+    console.log('Register page initialized');
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+    });
+  }
+}
